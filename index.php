3ae1d0846ac23edee24bff268d3906e4a8404eb2
Jon Chretien <jon.chretien@gmail.com>
2011-08-18 16:14:25 -0400

    modifying link colors and box shadow

--- index.php
+++ index.php
@@ -20,8 +20,8 @@
   <div id="welcome">
     <div class="inset">
       <h1>Static Format</h1>
-      <p id="summary">Static Format is the photography portfolio of Jon Chretien. The full site is currently being built, but in the meantime most of his work can be found on <a href="http://www.flickr.com/photos/truenotes">Flickr</a> and <a href="http://instagrid.me/jnchrtn/">Instagrid</a>.</p>
-      <p id="footer">Copyright &copy; <?php echo (date ("Y")) ?> Jon Chretien. All rights reserved.</p>
+      <p id="summary">Static Format is the photography portfolio of <a href="http://jonchretien.com/">Jon Chretien</a>. The full site is currently being built, but in the meantime most of his work can be found on <a href="http://www.flickr.com/photos/truenotes/">Flickr</a> and <a href="http://instagrid.me/jnchrtn/">Instagrid</a>.</p>
+      <p id="footer">Copyright &copy; <?php echo (date ("Y")) ?> <a href="http://jonchretien.com/">Jon Chretien</a>. All rights reserved.</p>
     </div>
   </div>
   
@@ -33,14 +33,14 @@
   </script>
 
   <script>
-    // var _gaq = _gaq || [];
-    //     _gaq.push(['_setAccount', 'UA-953377-2']);
-    //     _gaq.push(['_trackPageview']);
-    //     (function() {
-    //       var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    //       ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    //       var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-    //     })();
+    var _gaq = _gaq || [];
+    _gaq.push(['_setAccount', 'UA-953377-2']);
+    _gaq.push(['_trackPageview']);
+    (function() {
+      var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+      ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    })();
   </script>
   
 </body>
